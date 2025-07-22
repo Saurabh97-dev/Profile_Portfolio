3689adf317eb45e4fcfed27f989bca31d6da7f51
Saurabh97-dev <saurabhitdeveloper@gmail.com>
2025-07-22 16:31:11 +0530

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#007bff',
+        secondary: '#6c757d',
+      },
+    },
+  },
+  plugins: [],
+}
